76902af54253396769ba5524c1a71e459b916b3a
ajiajaa <raihanbarnanda@gmail.com>
2021-12-17 15:12:26 +0700

    faq final + index search

--- Homepage/js/scripts.js
+++ Homepage/js/scripts.js
@@ -5,3 +5,23 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(document).ready(function(){
+    $("#panel1").hide();
+  $("#flip1").click(function() 
+  {
+    $("#panel1").slideToggle();
+    $("#rotate").toggleClass('fa fa-angle-double-down fa fa-angle-double-up');
+    });
+    
+      $("#panel2").hide();
+$("#flip2").click(function(){
+    $("#panel2").slideToggle();
+    $("#rotate2").toggleClass('fa fa-angle-double-down fa fa-angle-double-up');
+    });
+
+    $("#panel3").hide();
+$("#flip3").click(function(){
+    $("#panel3").slideToggle();
+    $("#rotate3").toggleClass('fa fa-angle-double-down fa fa-angle-double-up');
+    });
+});
